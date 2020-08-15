1523573c4caf1993470d4c53797929e780bfeca9
benziv <9212261+benzgar@users.noreply.github.com>
2020-08-14 21:44:46 -0400

    Customized folder and files for Core (#3)
    JiraID-xxx
    
    Added gitignore
    Added do.sh file
    Updated scripts to build specifically for Core

--- scripts/ci_profiling.sh
+++ scripts/ci_profiling.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Run profiling
-cd $TRAVIS_BUILD_DIR/packages/bin
-./qure
-gprof ./qure > gprof.log
+cd $TRAVIS_BUILD_DIR/build/bin
+./console_app
+gprof ./console_app > gprof.log
 head gprof.log -n 100
--- scripts/profiling.sh
+++ scripts/profiling.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Run profiling
-cd ../packages/bin
-./qure
-gprof ./qure > gprof.log
+cd $BIN_DIR
+./console_app
+gprof ./console_app > gprof.log
 head gprof.log -n 100
+
+echo "Profiling finished!"
