34072a042e4193d60a57549a85b84cfef5bb09cb
benziv <9212261+benzgar@users.noreply.github.com>
2020-08-24 22:56:43 -0400

    Fix travis build (#7)
    PSCOR-xxxx
    
    Restore CI build script
    Fixed linker flag on scripts
    Removed unused lines

--- clean_build.sh
+++ clean_build.sh
@@ -8,7 +8,7 @@ done
 cd "$(dirname $0)" || exit 1
 CORE_CMAKE_FLAGS="-DBUILD_UNITTEST=OFF -DBUILD_ALL=ON $CORE_CMAKE_FLAGS"
 
-echo $CORE_CMAKE_FLAGS
+echo "cmake flags .. " $CORE_CMAKE_FLAGS
 
 rm -rf build
 mkdir build || exit 1
---
+++ scripts/ci_build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd $TRAVIS_BUILD_DIR
+./clean_build.sh -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=gold
