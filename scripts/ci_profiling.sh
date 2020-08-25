178aa71e4582500ef951692353e93d96982ba15f
benzgar <garcia.benziv@gmail.com>
2020-08-24 22:24:48 -0400

    Test travis

--- scripts/ci_profiling.sh
+++ scripts/ci_profiling.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Run profiling
-cd $TRAVIS_BUILD_DIR/build/bin
-./console_app
-gprof ./console_app > gprof.log
-head gprof.log -n 100
+cd $TRAVIS_BUILD_DIR
+
+./clean_build.sh -DCI_BUILD=ON
+
+# # Run profiling
+# cd $TRAVIS_BUILD_DIR/build/bin
+# ./console_app
+# gprof ./console_app > gprof.log
+# head gprof.log -n 100
