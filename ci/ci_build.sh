a69745e190117f489b0f151ac4a3c4e93d2844de
benzgar <garcia.benziv@gmail.com>
2020-09-05 19:25:04 -0400

    Adding gtest support

--- ci/ci_build.sh
+++ ci/ci_build.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-
-# Clone gtest
-cd $TRAVIS_BUILD_DIR
-mkdir external || exit 1
-cd external || exit 1
-echo Cloning gtest...
-git clone git@github.com:google/googletest.git
-
 # Start make build
 cd $TRAVIS_BUILD_DIR
 echo Starting clean build...
