2a1f81a02a9db7bcdc917a41edee07588f2006f2
benziv <9212261+benzgar@users.noreply.github.com>
2020-09-06 12:13:30 -0400

    Optimizing travis scripts (#12)
    * Optimizing travis scripts
    
    PCOR-xxx
    
    Using workspace feature to share the build bins
    Move installs to specific stages
    Adding automation to the console app

--- application/main.cpp
+++ application/main.cpp
@@ -7,6 +7,11 @@
 *                   Written by Ben Ziv <pointonsoftware@gmail.com>, August 2020                   *
 *                                                                                                 *
 **************************************************************************************************/
+
+/* NOTE!
+ * When updating the std::cin's of console_app, update ci/automation_input.txt as well.
+*/
+
 #include <iostream>
 #include <controller/authcontroller.hpp>
 #include <logger/loghelper.hpp>
--- ci/ci_profiling.sh
+++ ci/ci_profiling.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+# Run automation
 cd $TRAVIS_BUILD_DIR/build/bin
-./console_app
+./console_app < $TRAVIS_BUILD_DIR/ci/automation_input.txt
+
 # Run profiling
 gprof ./console_app > gprof.log
 head gprof.log -n 100
