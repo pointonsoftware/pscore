6740686193df593ecbf50b23239324b82871dfd0
benzgar <garcia.benziv@gmail.com>
2020-09-21 11:27:49 -0400

    Fixed cppcheck and unit test

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -3,5 +3,6 @@ export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && p
 cd $SCRIPT_DIR/../
 
 # Note: When updating the line below, update .travis.yml as well
-cppcheck --error-exitcode=1 --enable=all --suppress=missingIncludeSystem --inline-suppr \
+cppcheck --std=c++11 --enable=warning,style,performance,portability \
+ --error-exitcode=1 --inline-suppr \
  application databoundary invoker utility -iinvoker/unittest
