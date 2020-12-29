96e473fc6933459bcffa5ec34230ac5f595e716a
benzgar <garcia.benziv@gmail.com>
2020-12-29 17:15:19 -0500

    Fixed script directories

--- ci/ci_build.sh
+++ ci/ci_build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # Start make build
-cd $TRAVIS_BUILD_DIR
 echo Starting clean build...
 ./clean_build.sh -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=gold
--- ci/ci_profiling.sh
+++ ci/ci_profiling.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Run automation
-cd $TRAVIS_BUILD_DIR/build/bin
-./console_app < $TRAVIS_BUILD_DIR/ci/automation_input.txt
+./build/bin/console_app < ci/automation_input.txt
 
 # Run profiling
 gprof ./console_app > gprof.log
