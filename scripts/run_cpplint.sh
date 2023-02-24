5e8589503049f07d7262c3027b5b78487b0ec5ec
benziv <9212261+gbenziv@users.noreply.github.com>
2023-02-23 19:34:44 -0500

    Filter include_subdir from cpplint (#246)
    * Filter include_subdir from cpplint
    
    xxxx
    
    The project structure is intentionally designed to put the include
    and source files together in the same directory

--- scripts/run_cpplint.sh
+++ scripts/run_cpplint.sh
@@ -3,5 +3,5 @@ export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && p
 cd "$SCRIPT_DIR"/../
 
 # Note: When updating the line below, update .travis.yml as well
-cpplint --filter=-readability/multiline_comment,-whitespace/ending_newline,-build/c++11 --linelength=100 \
---recursive core orchestra mock utility
+cpplint --filter=-readability/multiline_comment,-whitespace/ending_newline,-build/c++11,-build/include_subdir \
+ --linelength=100 --recursive core orchestra mock utility
